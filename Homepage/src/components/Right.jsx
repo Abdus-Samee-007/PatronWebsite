4a54647db5b13b43b294760f546134e6cc23d4cb
Abdus Samee <abdus007samee@gmail.com>
2024-07-29 02:18:14 +0530

    Made the right col responsive

--- Homepage/src/components/Right.jsx
+++ Homepage/src/components/Right.jsx
@@ -4,7 +4,7 @@ import Card from './Card.jsx'
 function Right() {
   return (
     <div className='shsh flex '>
-    <div className='right text-white gap-20 mt-2 grid grid-cols-3 pl-20'>
+    <div className='right text-white gap-20 mt-2 grid lg:grid-cols-3 sm:grid-cols-2 pl-20'>
       <div className="card1 ">
     <Card />
       </div>
